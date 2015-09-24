814f2718625a28d8c54d8fc0c6092afceb0def8d
unknown <pusulurm@HYD-PUSULURM.ds.ad.adp.com>
2015-09-24 15:49:58 +0530

    Added a sample directive2

---
+++ module2.js
@@ -0,0 +1,7 @@
+/**
+ * Created by pusulurm on 9/24/2015.
+ */
+define(['./myDirective2'],function( directive2){
+    angular.module('Directive2Module', []).
+        directive('myDirective2', directive2)
+});
