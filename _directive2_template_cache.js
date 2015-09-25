7ee1fad7772a3a02c896e987f97e602516853895
pusulurm <murali.pusuluri@cdk.com>
2015-09-26 01:58:20 +0530

    ran html2js

--- _directive2_template_cache.js
+++ _directive2_template_cache.js
@@ -1,5 +1,5 @@
 angular.module('directive2.templates', []).run(['$templateCache', function($templateCache) {
   "use strict";
   $templateCache.put("../templates/directive2.html",
-    "<hr><h2>Hello from Directive 2</h2>{{message2}}<hr>");
+    "<hr><h2>Hello from Directive 2</h2>{{message2}}<label translate=DIRECTIVE2.KEY1></label><label translate=DIRECTIVE2.KEY2></label><label translate=DIRECTIVE2.KEY3></label><hr>");
 }]);
